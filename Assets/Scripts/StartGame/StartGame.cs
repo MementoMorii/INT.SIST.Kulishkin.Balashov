ff882878c9f4807ecbbb9be62b072777f395ecad
Alexey <a.kulishkin9@gmail.com>
2021-10-24 18:15:15 +0700

    Не поломанная

--- Assets/Scripts/StartGame/StartGame.cs
+++ Assets/Scripts/StartGame/StartGame.cs
@@ -7,9 +7,8 @@ public class StartGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube.AddComponent<Rigidbody>();
-        cube.transform.position = new Vector3(x, y, 0);
+        GameObject circle = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        circle.transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame

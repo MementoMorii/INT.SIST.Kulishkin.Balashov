fb5ad9ecbcd5a550afdd0f99e4121c90fa44af53
MementoMorii <b.garic.99@yandex.ru>
2021-10-24 19:23:40 +0300

    Добавил папку с проектом Cell в реп, и опять поменял игнор

---
+++ Cell/Interfases/IUnityObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cell.Interfases
+{
+    interface IUnityObject
+    {
+        public void Move(double angle, double speed);
+    }
+}

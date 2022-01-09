61e247ca01419d52564776a748a108d65341d96f
MementoMorii <b.garic.99@yandex.ru>
2022-01-09 21:55:26 +0300

    Добавил класс для реализации логики размножения.

---
+++ Assets/Scripts/Libraries/Reproduction/Reproduction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+public class Reproduction :  MonoBehaviour
+{
+    private HashSet<int> _allObjects = new HashSet<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+        
+    }
+}
+

4a809cf2e9370a0eacebd7945fade3e8aed1afa4
MementoMorii <b.garic.99@yandex.ru>
2022-01-09 21:54:57 +0300

    Добавил для наследника от класса Cell и переопределил в них методы столкновения и обнаружения объектов.

---
+++ Assets/Scripts/Libraries/Cell/EnemyCell.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyCell : Cell
+{
+    private HashSet<GameObject> _cellsInVision = new HashSet<GameObject>();
+
+    public override void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Cell":
+                _cellsInVision.Add(collision.gameObject);
+                break;
+            
+            default:
+                break;
+        }
+    }
+
+    virtual public void OnTriggerExit2D(Collider2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Cell":
+                _cellsInVision.Remove(collision.gameObject);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public override void OnCollisionEnter2D(Collision2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Cell":
+                Energy += 15;
+                //todo либо тут вызывать метод дамага, либо у самой клетки просто отнимать энергию.
+                break;
+
+            default:
+                break;
+
+        }
+    }
+}
+
---
+++ Assets/Scripts/Libraries/Cell/VeganCell.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class VeganCell : Cell
+{
+    private HashSet<GameObject> _foodInVision = new HashSet<GameObject>();
+    private HashSet<GameObject> _enemyInVision = new HashSet<GameObject>();
+
+    public override void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Food":
+                _foodInVision.Add(collision.gameObject);
+                break;
+
+            case "Enemy":
+                _enemyInVision.Add(collision.gameObject);
+                break;
+            
+            default:
+                break;
+        }
+
+    }
+
+    virtual public void OnTriggerExit2D(Collider2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Food":
+                _foodInVision.Remove(collision.gameObject);
+                break;
+
+            case "Enemy":
+                _enemyInVision.Remove(collision.gameObject);
+                break;
+            
+            default:
+                break;
+        }
+    }
+
+    public override void OnCollisionEnter2D(Collision2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Enemy":
+                //todo либо в классе Enemy прописать метод дамага, либо тут отниать у клетки энергию за столкновение с врагом.
+                break;
+
+            case "Food":
+                Energy += 15;
+                break;
+
+            default:
+                break;
+
+        }
+    }
+
+}
+
